4b60dfa91cb54451ed0dbae729f450b8b4a7f55c
Jeremiah de Guzman <124826126+jerrymiahdg@users.noreply.github.com>
2024-03-07 22:23:01 -0800

    Add files via upload

--- jdg-index.js
+++ jdg-index.js
@@ -18,3 +18,45 @@ for ([i, item] of jdgA.entries()) {
   valid[i] = true;
   item.addEventListener("click", createHandler(i));
 }
+
+///////////////////////////////////////////////
+
+const sideNav = document.querySelector(".jdg-side-nav");
+const sideNavWidth = sideNav.offsetWidth;
+const linksContainer = document.querySelector(".jdg-links-container");
+const menuBtn = document.querySelector(".jdg-nav-menu-btn");
+const body = document.querySelector("body");
+const closeCatcher = document.querySelector(".jdg-close-catcher");
+
+let screenWidth = window.innerWidth;
+
+sideNav.style = `transform: translateX(${sideNavWidth}px);`;
+closeCatcher.style = "display: none;";
+
+if (screenWidth <= 750) {
+  linksContainer.style = "display: none;";
+} else {
+  menuBtn.style = "display: none;";
+}
+
+window.addEventListener("resize", () => {
+  screenWidth = window.innerWidth;
+  if (screenWidth <= 750) {
+    linksContainer.style = "display: none;";
+    menuBtn.style = "";
+  } else {
+    linksContainer.style = "";
+    menuBtn.style = "display: none;";
+  }
+});
+
+menuBtn.addEventListener("click", () => {
+  sideNav.style = `transform: translateX(0px);`;
+  closeCatcher.style = "";
+});
+
+closeCatcher.addEventListener("click", () => {
+  console.log("alkjfalsjfal;kfdja");
+  sideNav.style = `transform: translateX(${sideNavWidth}px);`;
+  closeCatcher.style = "display: none;";
+});
